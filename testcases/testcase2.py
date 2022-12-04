eee8f824b907f16b24c0e70a4ad588eb34a95e05
geetinder.geetinder <geetinder.geetinder@consultant.volvo.com>
2022-12-04 13:52:09 +0200

    commit by sdet1

--- testcases/testcase2.py
+++ testcases/testcase2.py
@@ -1,2 +1,56 @@
-testcase2
-line added by sdet2
+"""
+This is a multiline 
+comment 
+"""
+print("start of testcase2")
+# name = input("Enter your name: ")
+# print ("Hello",name)
+#
+# age = input("Enter your age: ")
+# age = int(age)
+# print(name,", You are ",age," years old.")
+
+x="Hello"  #string
+print(x)
+
+#numeric
+x=10   #int
+print(x)
+x=10.2  #float
+print(x)
+x=1j  #complex
+print("complex:",x)
+
+x=["java","python","ruby"]   #list
+print("list:",x)
+x=("java","python","ruby")  #tuple
+print("Tuple:",x)
+
+x=range(10)
+print("range:",x)
+
+x={"name":"Tiya",
+   "job":"Scuba Instructor",
+   "id":10}
+print("Dictionary:",x)
+
+#set, frozenset
+x={"java","python","ruby"}
+print("set:",x)
+x=frozenset({"java","python","ruby"})
+print("frozen set:",x)
+x={"fruits","vegetables","meat"}
+print("set:",x)
+
+x=True
+print("Boolean:",x)
+
+print("end of testcase2")
+
+
+
+
+
+
+
+
--- testcases/testcase4.py
+++ testcases/testcase4.py
@@ -1,1 +1,34 @@
-sdet1 branch work
+x=10
+y='Geet'
+print(type(x),x)
+print(type(y),y)
+
+#typecasting
+x=str(5)
+y=int(5)
+z=float(5)
+print('x is stored as string with value:',x)
+print('y is stored as int with value:',y)
+print('z is stored as float with value:',z)
+
+#case sensitive
+x=5
+X=10
+print(x,' ',X)
+
+x,y,z="Orange","Banana","Cherry"
+print(x,' ',y," ",z)
+x=y=z="Grapes"
+print(x,' ',y," ",z)
+
+x="Python"
+y=" is great"
+print(x+y)
+
+x="7"
+y="5"
+print(x+y)
+
+x=7
+y=5
+print(x+y)
